051650f1d5f9f568dab77873c0fd5c8eb1baf6cf
ichigo_kurosaki <125087781+ichigo-k@users.noreply.github.com>
2024-10-24 00:28:41 +0000

    Added table schemas

---
+++ app/Models/Returned_Products.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Returned_Products extends Model
+{
+    /** @use HasFactory<\Database\Factories\ReturnedProductsFactory> */
+    use HasFactory;
+}
